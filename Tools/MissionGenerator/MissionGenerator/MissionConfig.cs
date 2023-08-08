a042de4439cd189d7b59173175ed977469778500
Liam Cannon <54691085+liamcannon@users.noreply.github.com>
2023-08-07 22:22:58 -0400

    added two compositions as an example

--- Tools/MissionGenerator/MissionGenerator/MissionConfig.cs
+++ Tools/MissionGenerator/MissionGenerator/MissionConfig.cs
@@ -9,7 +9,7 @@ namespace MissionGenerator;
 public class MissionConfig
 {
     public string Author { get; set; } = "91st Mission Builders";
-    public string Title { get; set; } = "91st MRCMission";
+    public string Title { get; set; } = "91st MRC Mission";
     public string Description { get; set; } = "A 91st MRC Mission File";
     public string Version { get; set; } = "V";
 }
